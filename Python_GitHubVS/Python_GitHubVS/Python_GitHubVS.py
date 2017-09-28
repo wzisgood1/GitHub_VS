4c1bf1bc81d9cefb02a3498bc1360385c92ade5e
Zhe Wang <zhewang.econ@outlook.com>
2017-09-28 17:27:31 +0800

    Add the paer of # Exercise 3

--- Python_GitHubVS/Python_GitHubVS/Python_GitHubVS.py
+++ Python_GitHubVS/Python_GitHubVS/Python_GitHubVS.py
@@ -8,3 +8,18 @@ print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
 
 print(100 + 200)
+
+
+# Exercise 3: Numbers and Math
+
+print("I will now count my chickens:")
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+# End of Exercise 3
